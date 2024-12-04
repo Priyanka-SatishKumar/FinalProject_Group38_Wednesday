a8b177107c44860ebd4166563fd24859fa6b2ce6
pjsk02 <pulipati.j@northeastern.edu>
2024-12-04 17:59:03 -0500

    Added new classes in model.pharmacy package

--- PharmaFlow/src/model/pharmacy/PharmacyPurchaseOrder.java
+++ PharmaFlow/src/model/pharmacy/PharmacyPurchaseOrder.java
@@ -4,10 +4,57 @@
  */
 package model.pharmacy;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  *
  * @author KAILASH
  */
 public class PharmacyPurchaseOrder {
+    private int orderId;
+    private final int pharmacyId;
+    private final int manufactureId;
+    private final Date purchaseOrderDate;
+    private String orderStatus;
+    private final List<PharmacyPurchaseOrderItem> orderItems = new ArrayList<>();
+
+    public PharmacyPurchaseOrder(int pharmacyId, int manufactureId, Date purchaseOrderDate) {
+        this.pharmacyId = pharmacyId;
+        this.manufactureId = manufactureId;
+        this.purchaseOrderDate = purchaseOrderDate;
+    }
     
+    public int getOrderId() {
+        return orderId;
+    }
+    
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+        
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+    
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public int getPharmacyId() {
+        return pharmacyId;
+    }
+
+    public int getPharmacymanufactureId() {
+        return manufactureId;
+    }
+
+    public Date getPurchaseOrderDate() {
+        return purchaseOrderDate;
+    }
+    
+    public List<PharmacyPurchaseOrderItem> getOrderItems() {
+        return orderItems;
+    }
 }
--- PharmaFlow/src/model/pharmacy/PharmacyPurchaseOrderItem.java
+++ PharmaFlow/src/model/pharmacy/PharmacyPurchaseOrderItem.java
@@ -4,10 +4,32 @@
  */
 package model.pharmacy;
 
+import model.common.Drug;
+
 /**
  *
  * @author KAILASH
  */
 public class PharmacyPurchaseOrderItem {
+    private final Drug drug;
+    private final float selling_price;
+    private final int quantity;
+
+    public PharmacyPurchaseOrderItem(Drug drug, int quantity, float selling_price) {
+        this.drug = drug;
+        this.quantity = quantity;
+        this.selling_price = selling_price;
+    }
+
+    public Drug getDrug() {
+        return drug;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
     
+    public float getSellingPrice() {
+        return selling_price;
+    }
 }
